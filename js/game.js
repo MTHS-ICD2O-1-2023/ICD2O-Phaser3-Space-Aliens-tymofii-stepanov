5081b21720dbb5ca42a9475efc227050cd00f353
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-04-18 18:29:32 +0000

    Done with work

---
+++ js/game.js
@@ -0,0 +1,31 @@
+/* global Phaser */
+
+// Copyright (c) 2020 Mr. Coxall ALL rights reserved
+//
+// Created by: Tymofii
+// This is the Phaser3 game configuration file
+
+/**
+ * Start Phase Game.
+ */
+const config = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1080,
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true,
+    },
+  },
+  // set background color
+  backgroundColor: 0x5f6e7a,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
+}
+
+const game = new Phaser.Game(config)
+console.log(game)
